ff771dec0f70a4e5a7f9815b0300f91e984bcd73
Leticia Orive Peña <letimedi17031994@gmail.com>
2021-02-22 18:40:26 +0100

    editar cinema

--- spring-proyecto4/spring-proyecto4/src/main/java/com/example/demo/entities/Cinema.java
+++ spring-proyecto4/spring-proyecto4/src/main/java/com/example/demo/entities/Cinema.java
@@ -10,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -26,6 +27,7 @@ public class Cinema implements Serializable{
 	
 	private String name;
 	
+
 	@OneToOne
 	@JoinColumn(name="id_direccion")
 	private Direction direction;
@@ -33,11 +35,32 @@ public class Cinema implements Serializable{
 	private String schedule;
 	
 	private Integer rooms;
+	private Integer postalCode;
 	
 	@ManyToMany(mappedBy="cinemas")
 	private List<Movie> movies = new ArrayList<>();
-	
+	private List<User> user =new ArrayList<>();
 	public Cinema() {}
+
+	public Cinema(String name, Direction direction, String schedule, Integer rooms, Integer postalCode,
+			List<Movie> movies, List<User> user) {
+		super();
+		this.name = name;
+		this.direction = direction;
+		this.schedule = schedule;
+		this.rooms = rooms;
+		this.postalCode = postalCode;
+		this.movies = movies;
+		this.user = user;
+	}
+
+	public Integer getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(Integer postalCode) {
+		this.postalCode = postalCode;
+	}
 
 	public Long getId() {
 		return id;
