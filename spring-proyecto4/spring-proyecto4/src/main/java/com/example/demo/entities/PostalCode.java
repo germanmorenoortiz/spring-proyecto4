4083287bdd3fe1337e43e4e0b073c37475b0edcf
oscar <germanmorenoortiz@gmail.com>
2021-03-04 18:13:38 +0100

    FINAL FINAL

---
+++ spring-proyecto4/spring-proyecto4/src/main/java/com/example/demo/entities/PostalCode.java
@@ -0,0 +1,23 @@
+package com.example.demo.entities;
+
+public class PostalCode {
+	String postalCode;
+
+	public PostalCode() {}
+
+	public PostalCode(String postalCode) {
+		super();
+		this.postalCode = postalCode;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+	
+		
+}
